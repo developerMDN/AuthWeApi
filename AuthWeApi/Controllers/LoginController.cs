4687a9060f068d60c2e1e0c31624278f12a5a5b8
John <developer.jn@gmail.com>
2019-07-09 16:38:59 -0500

    primer commit

---
+++ AuthWeApi/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using AuthWeApi.Controllers.Token;
+using AuthWeApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace AuthWeApi.Controllers
+{
+    /// <summary>
+    /// login controller para autenticar usuarios
+    /// </summary>
+    [AllowAnonymous]
+    [RoutePrefix("api/login")]
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [HttpGet]
+        [Route("echouser")]
+        public IHttpActionResult EchoUser()
+        {
+            var identity = Thread.CurrentPrincipal.Identity;
+            return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(UsuarioLoginRequest login)
+        {
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            //TODO: Validar credenciales, solo demo !!
+            bool isCredentialValid = (login.ClaveUsuario == "123456");
+            if (isCredentialValid)
+            {
+                var token = TokenGenerator.GenerateTokenJwt(login.NombreUsuario);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ AuthWeApi/Models/UsuarioLoginRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthWeApi.Models
+{
+    public class UsuarioLoginRequest
+    {
+        public string NombreUsuario { get; set; }
+
+        public string ClaveUsuario { get; set; }
+
+        public string CorreoElectronico { get; set; }
+
+        public int IdRol { get; set; }
+    }
+}
